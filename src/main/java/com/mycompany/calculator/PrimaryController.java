4e3fb298ee17ff74e289e63f5d641b1f26f661b4
Vasqjr <vasqjr@farmingdale.educlear>
2023-02-27 12:04:35 -0500

    Added General Equations (+, -, *, /) to Calculator.

--- src/main/java/com/mycompany/calculator/PrimaryController.java
+++ src/main/java/com/mycompany/calculator/PrimaryController.java
@@ -8,17 +8,77 @@ import javafx.util.*;
 
 public class PrimaryController {
 
-    String calcValue = "";
+    String calcString1 = "";
+    String calcString2 = "";
+    double value1 = 0.0;
+    double value2 = 0.0;
+    double answer = 0.0;
     
     @FXML
-    private Button buttonSeven;
+    private TextField calcField1;
     
     @FXML
-    private TextField calcField;
+    private TextField calcField2;
+    
+    @FXML
+    private TextField calcField3;
+    
+    @FXML
+    private TextField answerField;
     
     @FXML
     private void numberWrite(String v) throws IOException{
-        calcValue += v;
+        if(calcField2.getText() == ""){
+            calcString1 += v;
+            calcField1.setText(calcString1);
+        }
+        else{
+            calcString2 += v;
+            calcField3.setText(calcString2);
+        }
+    }
+    
+    @FXML
+    private void numberAdd(){
+        calcField2.setText("+");
+    }
+    
+    @FXML
+    private void numberMult(){
+        calcField2.setText("x");
+    }
+    
+    @FXML
+    private void numberDivide(){
+        calcField2.setText("/");
+    }
+    
+    @FXML
+    private void numberSub(){
+        calcField2.setText("-");
+    }
+    
+    @FXML
+    private void numberEquals(){
+        value1 = Double.parseDouble(calcString1);
+        value2 = Double.parseDouble(calcString2);
+        
+        if(calcField2.getText().equalsIgnoreCase("+") == true){
+            answer = value1 + value2;
+            answerField.setText(Double.toString(answer));
+        }
+        else if(calcField2.getText().equalsIgnoreCase("*") == true){
+            answer = value1 * value2;
+            answerField.setText(Double.toString(answer));
+        }
+        else if(calcField2.getText().equalsIgnoreCase("-") == true){
+            answer = value1 - value2;
+            answerField.setText(Double.toString(answer));
+        }
+        else if(calcField2.getText().equalsIgnoreCase("/") == true){
+            answer = value1 / value2;
+            answerField.setText(Double.toString(answer));
+        }
     }
     
     @FXML
